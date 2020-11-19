88f121da5b9083be312f7ebea903f5b49bde76bb
Pear_White <42296026+Pear-White@users.noreply.github.com>
2020-11-19 23:19:42 +0800

    Add files via upload

---
+++ hotzone_main_with_user_creation/project/hotzone/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(VirusDetail)
+admin.site.register(PatientDetail)
+admin.site.register(CaseDetail)
+admin.site.register(VisitedLocationDetail)
+admin.site.register(LocationDetail)
+admin.site.register(UserDetail)
