c9d30854c6da0cc7620e4efbbde18362284e91fd
kchan825 <kevinwai825@gmail.com>
2020-11-26 21:42:55 +0800

    clustering

--- hotzone_v1_2/project/hotzone/urls.py
+++ hotzone_v1_2/project/hotzone/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('viruses', views.list_viruses, name='list_viruses'),
     path('locations/<str:target>', views.show_location_detail, name='show_location_detail'),
     path('cases/<int:target>', views.show_case_detail, name='show_case_detail'),
-    path('viruses/<slug:target>', views.show_virus_detail, name='show_virus_detail'),
+    path('viruses/<str:target>', views.show_virus_detail, name='show_virus_detail'),
     path('cases/<int:target>/add_location', views.add_visted_location, name='add_visted_location'),
     path('user_creation', views.user_creation, name='user_creation'),
     path('', views.user_login, name='user_login'),
-    path('user_logout', views.user_logout, name='user_logout')
+    path('user_logout', views.user_logout, name='user_logout'),
+    path('clusters', views.view_clusters, name='view_clusters'),
 ]
--- hotzone_v1_2/project/hotzone/views.py
+++ hotzone_v1_2/project/hotzone/views.py
@@ -1,13 +1,62 @@
+from .models import LocationDetail, CaseDetail, VirusDetail, VisitedLocationDetail
+from .forms import AddForm, SearchBox, CustomUserCreationForm
 from django.shortcuts import render
 from django.contrib import messages
-from .models import LocationDetail, CaseDetail, VirusDetail, VisitedLocationDetail
-from .forms import AddForm, SearchBox
-import requests
 from django.contrib.auth.forms import UserCreationForm
-from .forms import CustomUserCreationForm
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from sklearn.cluster import DBSCAN
+from datetime import date
+import math
+import requests
+import numpy as np
+
+def custom_metric(q, p, space_eps, time_eps):
+    dist = 0
+    for i in range(2):
+        dist += (q[i] - p[i])**2
+    spatial_dist = math.sqrt(dist)
+
+    time_dist = math.sqrt((q[2]-p[2])**2)
+
+    if time_dist/time_eps <= 1 and spatial_dist/space_eps <= 1 and p[3] != q[3]:
+        return 1
+    else:
+        return 2
+
+
+def cluster(vector_4d, distance, time, minimum_cluster):
+
+    params = {"space_eps": distance, "time_eps": time}
+    db = DBSCAN(eps=1, min_samples=minimum_cluster-1, metric=custom_metric, metric_params=params).fit_predict(vector_4d)
+
+    unique_labels = set(db)
+    total_clusters = len(unique_labels) if -1 not in unique_labels else len(unique_labels) -1
+
+    print("Total clusters:", total_clusters)
+
+    total_noise = list(db).count(-1)
+
+    print("Total un-clustered:", total_noise)
+
+    clusters = []
+
+    for k in unique_labels:
+        if k != -1:
+
+            labels_k = db == k
+            cluster_k = vector_4d[labels_k]
+
+            pts = []
+            for pt in cluster_k:
+                pts.append({'x': pt[0], 'y': pt[1], 'day': pt[2], 'caseNo': pt[3]})
+            
+            cluster = {'number': k+1, 'size': len(cluster_k), 'pts': pts}
+            clusters.append(cluster)
+
+    result = {'total_clusters': total_clusters, 'total_noise': total_noise, 'clusters': clusters}
+    return result
 
 def user_creation(request): 
     if request.user.is_authenticated:
@@ -172,3 +221,23 @@ def add_visted_location(request, target):
     context = {'case': case, 'add_form': add_form, 'hotzone_results_list': hotzone_results_list, 'api_results_list': api_results_list, 'message': message}
 
     return render(request, 'add_visited_location.html', context)
+
+@login_required(login_url='user_login')
+def view_clusters(request):
+    context = {'flag': -1}
+    if "view" in request.POST:
+        dots_list = []
+        visitedLocations = VisitedLocationDetail.objects.all()
+        for visitedLocation in visitedLocations:
+            locationDetail = LocationDetail.objects.get(name=visitedLocation.name)
+            d0 = date(2020, 1, 1)
+            delta = visitedLocation.dateFrom - d0
+            dots_list.append([locationDetail.xCoord, locationDetail.yCoord, delta.days, visitedLocation.caseNumber.caseNumber])
+        dots_array = np.array(dots_list)
+        D = int(request.POST.get("distance"))
+        T = int(request.POST.get("time"))
+        C = int(request.POST.get("size"))
+        result = cluster(dots_array, D, T, C)
+        context = {'result': result, 'flag': 1}
+
+    return render(request, 'view_clusters.html', context)
